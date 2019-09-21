2b924b5e44724ad7b80b9be92de7170d042934a7
Shaunelle T <shaunella_thomas@yahoo.com>
2019-09-20 20:42:00 -0400

    Main created

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import readInput.ReadInputFile;
+
+class Main {
+  public static void main(String[] args) {
+     System.out.println("\n\n\t\t\t 𝓐𝓼𝓬𝓮𝓷𝓭𝓲𝓷𝓰  / 𝓓𝓮𝓼𝓬𝓮𝓷𝓭𝓲𝓷𝓰 𝓐𝓻𝓻𝓪𝔂 ?\n\n");
+    ReadInputFile file = new ReadInputFile();
+    file.orderOfInputFile();
+  }
+}
